ee0223bc71fc054e9dd7eb78723ee7ce5f75b95c
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-07-30 06:12:27 -0300

    feat: add EnumHelper GetDescription

---
+++ Helpers/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonSolution.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription<TEnum>(TEnum value) where TEnum : Enum
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attr = field?.GetCustomAttribute<DescriptionAttribute>();
+            return attr?.Description ?? value.ToString();
+        }
+    }
+}

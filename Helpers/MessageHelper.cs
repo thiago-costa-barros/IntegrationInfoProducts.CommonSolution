c7df1ce439247efc3f16624bf12d5a298570a400
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-07-30 06:40:44 -0300

    feat: add MessageHelper

---
+++ Helpers/MessageHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CommonSolution.Helpers
+{
+    public static class MessageHelper
+    {
+        /// <summary>
+        /// Gera mensagem com base em enum e recurso associado ao DisplayAttribute.
+        /// </summary>
+        public static string FormatFromEnum<TEnum>(TEnum enumValue, Func<string, string> resourceAccessor, string template)
+            where TEnum : Enum
+        {
+            var displayAttr = enumValue.GetType()
+                .GetField(enumValue.ToString())?
+                .GetCustomAttribute<DisplayAttribute>();
+
+            if (displayAttr == null)
+                throw new InvalidOperationException($"Enum {enumValue} não possui DisplayAttribute.");
+
+            var name = displayAttr.GetName(); // "HWB0001", etc.
+            var displayValue = resourceAccessor(name); // HotmartMessages.HWB0001, etc.
+
+            return string.Format(template, displayValue);
+        }
+    }
+}

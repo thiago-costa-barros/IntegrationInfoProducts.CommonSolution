a8c61d974a12f3eae68f0df7a714dfa2e4ff1a9f
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-03 09:00:16 -0300

    feat: add DefaultUserService

---
+++ Entities/DefaultUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonSolution.Entities
+{
+    public class DefaultUserService
+    {
+        public int DefaultUserId { get; set; }
+        public string? DefaultUserName { get; set; }
+        public Guid Identifier { get; set; }
+    }
+}

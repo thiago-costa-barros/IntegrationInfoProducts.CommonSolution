ee3f8e0f440770885193816ce78d5982193afabf
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-08 07:38:26 -0300

    feat: add validation helper

---
+++ Helpers/ValidationHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace CommonSolution.Helpers;
+public static class ValidationHelper
+{
+    public static string GetJsonPropertyName<T>(string propertyName)
+    {
+        var prop = typeof(T).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+        if (prop == null) return propertyName;
+
+        var jsonAttr = prop.GetCustomAttribute<JsonPropertyNameAttribute>();
+        return jsonAttr?.Name ?? propertyName;
+    }
+}

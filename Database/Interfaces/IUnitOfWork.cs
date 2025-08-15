acbd390253ff004cc2dfd40ec2a4ef0466edf5d8
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-15 11:04:50 -0300

    feat: add interface UnitOfWork

---
+++ Database/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace CommonSolution.Database.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync(IsolationLevel isolation = IsolationLevel.ReadCommitted, CancellationToken ct = default);
+        Task CommitAsync(CancellationToken ct = default);
+        Task RollbackAsync(CancellationToken ct = default);
+        Task ExecuteResilientAsync(Func<CancellationToken, Task> action, CancellationToken ct = default);
+    }
+}

7b1babaeb9fa01600f720981650b96c56264a1f3
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-16 14:47:48 -0300

    refactor: nome dos contratos

--- Database/Interfaces/IUnitOfWork.cs
+++ Database/Interfaces/IUnitOfWork.cs
@@ -4,9 +4,9 @@ namespace CommonSolution.Database.Interfaces
 {
     public interface IUnitOfWork
     {
-        Task BeginTransactionAsync(IsolationLevel isolation = IsolationLevel.ReadCommitted, CancellationToken ct = default);
-        Task CommitAsync(CancellationToken ct = default);
-        Task RollbackAsync(CancellationToken ct = default);
-        Task ExecuteResilientAsync(Func<CancellationToken, Task> action, CancellationToken ct = default);
+        Task BeginTransaction(CancellationToken ct = default);
+        Task Commit(CancellationToken ct = default);
+        Task Rollback(CancellationToken ct = default);
+        Task ExecuteResilient(Func<CancellationToken, Task> action, CancellationToken ct = default);
     }
 }

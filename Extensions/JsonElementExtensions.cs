a8809ca294074ecee4377717928000cf7b734fc5
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-16 14:48:01 -0300

    feat: add extensions

---
+++ Extensions/JsonElementExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace CommonSolution.Extensions
+{
+    public static class JsonElementExtensions
+    {
+        public static T? MapToObject<T>(this JsonElement? element)
+        {
+            if (element == null || element.Value.ValueKind == JsonValueKind.Null)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(element.Value.GetRawText(), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}

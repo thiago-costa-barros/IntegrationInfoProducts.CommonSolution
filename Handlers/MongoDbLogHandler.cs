62409aec382991f77a4ad0d44afc01b730e3d070
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-10-04 16:32:58 -0300

    feat: add handlers

---
+++ Handlers/MongoDbLogHandler.cs
@@ -0,0 +1,15 @@
+﻿using CommonSolution.Entities.Logging;
+using CommonSolution.Interfaces.Logging;
+
+namespace CommonSolution.Handlers
+{
+    public class MongoDbLogHandler : ILogHandler
+    {
+        public string ProviderName => "MongoDb";
+
+        public Task LogAsync(LogEntry logEntry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
